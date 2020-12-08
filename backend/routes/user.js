735fb995093e47897a4cadc93961d7b38907c021
Jean <jeanlabedwcs@gmail.com>
2020-12-08 16:50:36 +0100

    fix gitignore

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userCtrl = require("../controllers/user");
-
-const router = express.Router();
-
-// Routing
-router.post("/signup", userCtrl.signup); // Create new User
-router.post("/login", userCtrl.login); // Connect User
-module.exports = router;
